a2b4edfd5d4224297a2ad2c9befdb33a0b73e8e4
patrick brisbin <pbrisbin@gmail.com>
2020-11-09 08:54:58 -0500

    Don't require PATHs
    Design errors out of existence: doing nothing for no paths is perfectly
    reasonable.

--- app/Main.hs
+++ app/Main.hs
@@ -21,4 +21,4 @@ options =
         <$> (not <$> switch (long "no-remove-spaces" <> help "Don't remove trailing spaces"))
         <*> (not <$> switch (long "no-fix-newlines" <> help "Don't fix ending newlines"))
         <*> switch (long "strict" <> help "Abort on exceptions")
-        <*> some (argument str (metavar "PATH" <> help "File to fix (inplace)"))
+        <*> many (argument str (metavar "PATH" <> help "File to fix (inplace)"))
