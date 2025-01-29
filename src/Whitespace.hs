33913c6b1d36034f0ac5bd8129fc7569422bb6bc
patrick brisbin <pbrisbin@gmail.com>
2025-01-29 11:55:35 -0500

    chore(style): fourmolu

--- src/Whitespace.hs
+++ src/Whitespace.hs
@@ -1,54 +1,59 @@
 module Whitespace
-    ( FormatOptions(..)
-    , formatPaths
-    , formatPath
-    , format
-
-    -- Exported for testing error-handling
-    , UnableToFormat(..)
-    )
+  ( FormatOptions (..)
+  , formatPaths
+  , formatPath
+  , format
+  -- Exported for testing error-handling
+  , UnableToFormat (..)
+  )
 where
 
 import RIO
 
 import RIO.Char (isSpace)
-import qualified RIO.Text as T
+import RIO.Text qualified as T
 
 data FormatOptions = FormatOptions
-    { foSpaces :: Bool -- ^ Trim trailing whitespace from lines?
-    , foNewlines :: Bool -- ^ Fix newlines at end of file?
-    , foStrict :: Bool -- ^ Halt on errors reading files?
-    , foPaths :: [FilePath] -- ^ Files to process
-    }
+  { foSpaces :: Bool
+  -- ^ Trim trailing whitespace from lines?
+  , foNewlines :: Bool
+  -- ^ Fix newlines at end of file?
+  , foStrict :: Bool
+  -- ^ Halt on errors reading files?
+  , foPaths :: [FilePath]
+  -- ^ Files to process
+  }
 
 formatPaths
-    :: (MonadUnliftIO m, MonadReader env m, HasLogFunc env)
-    => FormatOptions
-    -> m ()
+  :: (MonadUnliftIO m, MonadReader env m, HasLogFunc env)
+  => FormatOptions
+  -> m ()
 formatPaths opts = for_ (foPaths opts) $ \path ->
-    handleAny (handleErr (foStrict opts) path) $ formatPath opts path
+  handleAny (handleErr (foStrict opts) path) $ formatPath opts path
 
 data UnableToFormat
-    = UnableToFormatCRLF
-    | UnableToRead SomeException -- ^ Most likely non-Utf8
-    deriving stock Show
-    deriving anyclass Exception
+  = UnableToFormatCRLF
+  | -- | Most likely non-Utf8
+    UnableToRead SomeException
+  deriving stock (Show)
+  deriving anyclass (Exception)
 
 formatPath :: MonadUnliftIO m => FormatOptions -> FilePath -> m ()
 formatPath opts path = do
-    content <- readFileUtf8 path `catchAny` (throwIO . UnableToRead)
-    if isCRLF content
-        then throwIO UnableToFormatCRLF
-        else writeFileUtf8 path $ format opts content
+  content <- readFileUtf8 path `catchAny` (throwIO . UnableToRead)
+  if isCRLF content
+    then throwIO UnableToFormatCRLF
+    else writeFileUtf8 path $ format opts content
 
 isCRLF :: Text -> Bool
 isCRLF = ("\r\n" `T.isInfixOf`)
 
 format :: FormatOptions -> Text -> Text
 format opts t
-    | T.null t = t
-    | otherwise = onOpt foNewlines newlines $ onOpt foSpaces spaces t
-    where onOpt attr f = bool id f $ attr opts
+  | T.null t = t
+  | otherwise = onOpt foNewlines newlines $ onOpt foSpaces spaces t
+ where
+  onOpt attr f = bool id f $ attr opts
 
 -- | Ensure a single trailing newline
 newlines :: Text -> Text
@@ -62,20 +67,20 @@ eachLine :: (Text -> Text) -> Text -> Text
 eachLine f = T.unlines . map f . T.lines
 
 handleErr
-    :: (MonadIO m, MonadReader env m, HasLogFunc env, Display ex)
-    => Bool
-    -> FilePath
-    -> ex
-    -> m ()
+  :: (MonadIO m, MonadReader env m, HasLogFunc env, Display ex)
+  => Bool
+  -> FilePath
+  -> ex
+  -> m ()
 handleErr strict path ex
-    | strict
-    = do
+  | strict =
+      do
         logError
-            $ "Exception processing "
-            <> fromString path
-            <> ":"
-            <> display ex
-            <> ", aborting (disable strict mode to ignore)"
+          $ "Exception processing "
+          <> fromString path
+          <> ":"
+          <> display ex
+          <> ", aborting (disable strict mode to ignore)"
         exitWith $ ExitFailure 1
-    | otherwise
-    = logWarn $ "Exception processing " <> fromString path <> ":" <> display ex
+  | otherwise =
+      logWarn $ "Exception processing " <> fromString path <> ":" <> display ex
--- test/WhitespaceSpec.hs
+++ test/WhitespaceSpec.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS_GHC -Wno-missing-local-signatures #-}
 
 module WhitespaceSpec
-    ( spec
-    )
+  ( spec
+  )
 where
 
 import RIO
@@ -12,60 +12,64 @@ import Whitespace
 
 spec :: Spec
 spec = do
-    let
-        opts = FormatOptions
-            { foSpaces = True
-            , foNewlines = True
-            , foStrict = True -- Unused
-            , foPaths = [] -- Unused
-            }
+  let opts =
+        FormatOptions
+          { foSpaces = True
+          , foNewlines = True
+          , foStrict = True -- Unused
+          , foPaths = [] -- Unused
+          }
 
-    describe "formatPath" $ do
-        it "doesn't incorrectly format non-utf8" $ do
-            let path = "test/fixtures/non-utf8.yaml"
+  describe "formatPath" $ do
+    it "doesn't incorrectly format non-utf8" $ do
+      let path = "test/fixtures/non-utf8.yaml"
 
-            runSimpleApp (formatPath opts path) `shouldThrow` \case
-                UnableToRead _ -> True
-                _ -> False
+      runSimpleApp (formatPath opts path) `shouldThrow` \case
+        UnableToRead _ -> True
+        _ -> False
 
-    describe "format" $ do
-        it "strips trailing whitespace from the given content" $ do
-            let content = mconcat
-                    [ "line one  "
-                    , "\nline two "
-                    , "\n "
-                    , "\nline three \\" -- preserved
-                    , "\n"
-                    ]
-                expected =
-                    mconcat
-                        [ "line one"
-                        , "\nline two"
-                        , "\n"
-                        , "\nline three \\"
-                        , "\n"
-                        ]
+  describe "format" $ do
+    it "strips trailing whitespace from the given content" $ do
+      let
+        content =
+          mconcat
+            [ "line one  "
+            , "\nline two "
+            , "\n "
+            , "\nline three \\" -- preserved
+            , "\n"
+            ]
+        expected =
+          mconcat
+            [ "line one"
+            , "\nline two"
+            , "\n"
+            , "\nline three \\"
+            , "\n"
+            ]
 
-            format opts content `shouldBe` expected
-            format opts { foSpaces = False } content `shouldBe` content
+      format opts content `shouldBe` expected
+      format opts {foSpaces = False} content `shouldBe` content
 
-        it "strips extra newlines from the end of the content" $ do
-            let content =
-                    mconcat
-                        [ "line one"
-                        , "\nline two"
-                        , "\n"
-                        , "\n"
-                        , "\nline three"
-                        , "\n"
-                        , "\n"
-                        , "\n"
-                        ]
-                expected = mconcat
-                    ["line one", "\nline two", "\n\n", "\nline three", "\n"]
+    it "strips extra newlines from the end of the content" $ do
+      let
+        content =
+          mconcat
+            [ "line one"
+            , "\nline two"
+            , "\n"
+            , "\n"
+            , "\nline three"
+            , "\n"
+            , "\n"
+            , "\n"
+            ]
+        expected =
+          mconcat
+            ["line one", "\nline two", "\n\n", "\nline three", "\n"]
 
-            format opts content `shouldBe` expected
-            format opts { foNewlines = False } content `shouldBe` content
+      format opts content `shouldBe` expected
+      format opts {foNewlines = False} content `shouldBe` content
 
-        it "does not affect completely empty files" $ do
-            format opts "" `shouldBe` ""
+    it "does not affect completely empty files" $ do
+      format opts "" `shouldBe` ""
